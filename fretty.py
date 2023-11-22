4f2f561316fe436ad6ee94b6d65b2d0c57a4c683
Thomas Volk (MacBook) <info@thomasvolk.de>
2023-11-22 12:12:27 +0100

    add first complete implementation

--- fretty.py
+++ fretty.py
@@ -2,27 +2,30 @@
 
 from dataclasses import dataclass
 
+@dataclass
+class Note:
+    position: int 
+    value: str
 
+    
 class FBString:
-    def __init__(self, line):
-        self.notes = dict()
+    def __init__(self, position, line):
+        self.position = position
+        self.notes = []
         self.frets = 0
         for position, c in enumerate(line.strip()):
             self.frets += 1
             if c != '-':
-                self.notes[position]= c
-    
-    def is_note(self, position):
-        return self.notes.has_key(position)
+                self.notes.append(Note(position, c))
     
     def __repr__(self):
-        return f"FBString({self.notes.keys()})"
+        return f"FBString({self.position}, {self.notes})"
 
 
 class Fretboard:
     def __init__(self, lines):
-        self.position = lines[0]
-        self.strings = [ FBString(line) for line in lines[1:] ]
+        self.start_position = lines[0].strip()
+        self.strings = [ FBString(pos, line) for pos, line in enumerate(lines[1:]) ]
 
     @property
     def string_count(self):
@@ -40,7 +43,9 @@ class Fretboard:
 class ViewConfig:
     string_distance: int = 40
     fret_distance: int = 60
-    margin: int = 20
+    margin: int = 10
+    note_radius: int = 15
+    start_position_space: int = 40
 
 
 class SvgGenerator:
@@ -50,11 +55,15 @@ class SvgGenerator:
      xmlns:svgjs="http://svgjs.com/svgjs" 
      preserveAspectRatio="xMidYMid meet"
      viewBox="0 0 {width} {height}">
-{string_lines}
-{fret_lines}
+{start_position}
+{frets}
+{strings}
+{notes}
 </svg>"""
     line_template = '<line x1="{start_x}" y1="{start_y}"  x2="{end_x}" y2="{end_y}"  stroke-width="2" stroke="#000000"></line>'
-    
+    note_template = '<circle r="{radius}" cx="{x}" cy="{y}" fill="#000000" stroke-width="0" stroke="#000000"></circle>'
+    start_position_template = '<text x="{x}" y="{y}" font-family="Arial" font-size="{size}" text-anchor="middle" dominant-baseline="central" fill="#000000">{position}</text>'
+
     def __init__(self, view_config):
         self.view_config = view_config
 
@@ -62,25 +71,47 @@ class SvgGenerator:
         cfg = self.view_config
         width = fretboard.fret_count * cfg.fret_distance + (2 * cfg.margin)
         height = fretboard.string_count * cfg.string_distance + (2 * cfg.margin)
-        string_lines = '\n'.join([ self.line_template.format(
-                            start_y=i*cfg.string_distance + cfg.margin,
-                            end_y=i*cfg.string_distance + cfg.margin,
-                            start_x=cfg.margin,
-                            end_x=width - cfg.margin
-                         ) 
-                         for i in range(0, fretboard.string_count + 1) ])
-        fret_lines = '\n'.join([ self.line_template.format(
-                            start_y=cfg.margin,
-                            end_y=height - cfg.margin,
-                            start_x=i*cfg.fret_distance + cfg.margin,
-                            end_x=i*cfg.fret_distance + cfg.margin
-                         ) 
-                         for i in range(0, fretboard.fret_count + 1) ])
+        y_offest = cfg.margin + cfg.start_position_space
+        strings = '\n'.join([ 
+            self.line_template.format(
+                start_y=i*cfg.string_distance + y_offest,
+                end_y=i*cfg.string_distance + y_offest,
+                start_x=cfg.margin,
+                end_x=width - cfg.margin
+            ) 
+            for i in range(0, fretboard.string_count) 
+        ])
+        frets = '\n'.join([ 
+            self.line_template.format(
+                start_y=y_offest,
+                end_y=height - cfg.margin,
+                start_x=i*cfg.fret_distance + cfg.margin,
+                end_x=i*cfg.fret_distance + cfg.margin
+            ) 
+            for i in range(0, fretboard.fret_count + 1) 
+        ])
+        notes = '\n'.join([
+            self.note_template.format(
+                radius=cfg.note_radius,
+                x=n.position * cfg.fret_distance + cfg.margin + (cfg.fret_distance/2),
+                y=s.position * cfg.string_distance + y_offest
+            )
+            for s in fretboard.strings for n in s.notes
+        ])
+        start_position = self.start_position_template.format(
+            x=cfg.margin + (cfg.start_position_space/2),
+            y=cfg.margin + (cfg.start_position_space/2),
+            size=cfg.start_position_space,
+            position=fretboard.start_position
+        )
+
         return self.main_template.format(
                     width=width, 
                     height=height, 
-                    string_lines=string_lines,
-                    fret_lines=fret_lines
+                    start_position=start_position,
+                    frets=frets,
+                    strings=strings,
+                    notes=notes
                 )
 
 
